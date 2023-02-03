8a7b00cb353a19b83fa4cd2545ccc1cf57d429d7
Diego Araújo Borges da Silva <didoarorges@gmail.com>
2023-02-03 20:37:01 -0300

    Project 4 started!

---
+++ EasyBrowser/EasyBrowser/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by Diego Araújo Borges on 03/02/23.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+
+
+}
+
